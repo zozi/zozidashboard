31cf15351ca7e6805101c468593c4c8781a48605
Skylar Brown <skylar.brown@zozi.com>
2015-12-08 18:18:40 -0800

    Memory widgets don't crash when a server is unavailable / offline, github widget properly shows newest PR

--- jobs/memory.rb
+++ jobs/memory.rb
@@ -6,7 +6,7 @@ require 'rest_client'
 
 url = 'https://api.newrelic.com/v2/servers.json?filter[ids]=1557588,+2709321,+2514170,+2297767,+2709479'
 
-SCHEDULER.every '30s' do
+SCHEDULER.every '10s' do
 	response = RestClient.get(
 		url,
 		:'X-Api-Key' => ENV['NEWRELIC_API_KEY']
@@ -14,13 +14,14 @@ SCHEDULER.every '30s' do
 	server_data = JSON.parse(response)
 	servers = server_data["servers"]
 	meters = []
-	servers.map.with_index do |server|
-		meter_data = {		
-			name: server["name"],
-			value: server["summary"]["memory"]
-		}
-		meters << meter_data
-		#return meters
+	servers.each_with_index do |server, index|
+		if server["summary"] != nil
+			meter_data = {		
+				name: server["name"],
+				value: server["summary"]["memory"]
+			}
+			meters << meter_data
+		end
 	end
 	meters.each_with_index do |meter, index|
 		send_event("memory"+(index+1).to_s,   { value: meters[index][:value], moreinfo: meters[index][:name] })
--- jobs/pullrequests.rb
+++ jobs/pullrequests.rb
@@ -6,18 +6,17 @@ url = ENV['GITHUB_PULLS_URL']
 access_token = ENV['GITHUB_API_TOKEN']
 
 pull_requests = Hash.new({ value: 0 })
-max_shown = 16
+max_shown = 15
 
 SCHEDULER.every '1m' do
 	response = RestClient.get(url+"?access_token="+access_token)
 	pr_data = JSON.parse(response)
-	#puts pr_data[0]["title"]
-	for index in (1..max_shown) do
+	for index in (0..max_shown) do
 		pull_requests[pr_data[index]["title"]] = { label: pr_data[index]["title"], value: pr_data[index]["user"]["login"]}
 		#puts pr_data[index]["user"]["login"]
 	end
 	if(pr_data.length > max_shown)
-		pull_requests["Number not shown"] = { label: (pr_data.length - max_shown).to_s + " more not shown", value: ""}
+		pull_requests["ZZZZZZZZZ"] = { label: (pr_data.length - max_shown).to_s + " more not shown", value: ""}
 	end
 	send_event('pullrequests', { items: pull_requests.values })
 end
